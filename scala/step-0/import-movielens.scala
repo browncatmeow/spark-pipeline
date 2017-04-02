e697f33ea12594d5cc52846f1c864f2231d4281b
browncatmeow <prolink8000@gmail.com>
2017-04-02 18:56:49 +0800

    Update import-movielens.scala

--- scala/step-0/import-movielens.scala
+++ scala/step-0/import-movielens.scala
@@ -1,3 +1,7 @@
+// MAGIC %md # Step 0: Import your data
+// MAGIC 
+// MAGIC Use the movielens dataset provided with Spark.
+
 val sqlContext= new org.apache.spark.sql.SQLContext(sc)
 import sqlContext.implicits._
 
@@ -8,12 +12,13 @@ import org.apache.spark.sql.types._
 import spark.implicits._
 import spark.sql
 
+// COMMAND ----------
+
+// If you are under $SPARK_HOME, you can just use "data/mllib/sample_movielens_data.txt" as textFile source
 val rdd = sc.textFile("/Users/wt/Desktop/spark-2.1.0-bin-hadoop2.7/data/mllib/sample_movielens_data.txt").map { line =>
 
 val Array(user, item, rating) = line.split("::")
-
   (user, item, rating)
-
 }
 
 val df = sqlContext.createDataFrame(rdd).toDF("user", "item", "rating")
